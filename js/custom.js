bc7ad4d2110579f65cf7b447a46435cbd03a47fa
vigdraw <vigdraw@gmail.com>
2025-04-22 19:59:11 +0800

    Site updated: 2025-04-22 19:59:10

--- js/custom.js
+++ js/custom.js
@@ -17,7 +17,7 @@ function show_runtime() {
   c = (b - B) * 60;
   C = Math.floor((b - B) * 60);
   D = Math.floor((c - C) * 60);
-  document.getElementById("runtime_span").innerHTML = "本站已运行: " + A + "天" + B + "小时" + C + "分" + D + "秒";
+  document.getElementById("runtime_span").innerHTML = "本站已运行 " + A + "天" + B + "小时" + C + "分" + D + "秒";
 }
 show_runtime();
 
